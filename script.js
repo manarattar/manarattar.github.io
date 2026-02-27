056798691bec3becca3c0a0a571eab75b140f2c3
manarattar <manarattar77@gmail.com>
2026-02-27 03:13:36 +0100

    Add fade-in effect on scroll using IntersectionObserver

---
+++ script.js
@@ -0,0 +1,20 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
